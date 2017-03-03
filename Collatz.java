c59af21def6150204ae88afbed7083851c6741c5
Joseph Nuhfer <nuhferjc@appstate.edu>
2017-03-02 20:22:21 -0500

    Fixed brackets.

--- Collatz.java
+++ Collatz.java
@@ -6,38 +6,30 @@ import java.util.Scanner;
  * Given a range of integers a to b inclusive, where a <= b and a, b >= 1, this program calculates the integer with the largest Collatz length.
  */
 
-public class Collatz
-{
+public class Collatz {
     private static int[] resultTable;
 
-    private static int collatzLength(long x)
-    {
-        if (x < 100000000)
-        {    
-            if (resultTable[(int) x] != 0)
-            {
+    private static int collatzLength(long x) {
+        if (x < 100000000) {    
+            if (resultTable[(int) x] != 0) {
                 return resultTable[(int) x];
             }
-            if (x % 2 == 0)
-            {
+            if (x % 2 == 0) {
                 resultTable[(int) x] = collatzLength(x / 2) + 1;
                 return resultTable[(int) x];
             }
             resultTable[(int) x] = collatzLength(3 * x + 1) + 1;
             return resultTable[(int) x];
         }
-        else
-        {
-            if (x % 2 == 0)
-            {
+        else {
+            if (x % 2 == 0) {
                 return collatzLength(x / 2) + 1;
             }
             return collatzLength(3 * x + 1) + 1;
         }
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         resultTable = new int[100000000];
         resultTable[1] = 1;
         Scanner keyboard = new Scanner(System.in);
@@ -46,15 +38,12 @@ public class Collatz
         long b = keyboard.nextInt();
         long maxCollatzLength = 0;
         long maxInRange = 0;
-        if (a > b)
-        {
+        if (a > b) {
             System.out.println("Invalid input");
             return;
         }
-        for (long i = a; i <= b; i++)
-        {
-            if (collatzLength(i) > maxCollatzLength)
-            {
+        for (long i = a; i <= b; i++) {
+            if (collatzLength(i) > maxCollatzLength) {
                 maxCollatzLength = collatzLength(i);
                 maxInRange = i;
             }
